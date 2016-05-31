ae00d915c045e7063e5a4d8093209879f38dc407
patnir <rahul20patni@gmail.com>
2016-05-31 09:12:55 -0400

    Too many records exception

---
+++ clsTooManyRecordsException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class clsTooManyRecordsException : ApplicationException
+{
+    public clsTooManyRecordsException(string message)
+        : base(message)
+    {
+
+    }
+}
